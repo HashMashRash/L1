6112b8dae31004273addb5be9603562578779c0a
Kirill <hashmashrash@mail.ru>
2019-05-14 19:21:32 +0300

    измененный комит

--- Lesson 1/JS/script.js
+++ Lesson 1/JS/script.js
@@ -1,27 +1,21 @@
-let money = prompt("Ваш бюджет на месяц?");
-let time = prompt("Введите дату в формате YY-MM-DD");
+let money = prompt("Ваш бюджет на месяц?", ''),
+	time = prompt('Введите дату в формате YYYY-MM-DD', '');
 
 let appData = {
-    budget: money,
-    timeData: time,
-    expenses: {
-        q1: prompt("Введите обязательную статью расходов в этом месяце"),
-        q2: prompt("Во сколько обойдется?"),
-        q3: prompt("Введите обязательную статью расходов в этом месяце"),
-        q4: prompt("Во сколько обойдется?"),
-
-       
-
-    },
-
-    optionalExpenses: {},
-    income: [],
-    savings: false,
-
-
+	budget: money,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	timeData: time,
+	savings: false
 };
 
-appData.expenses.q1 = appData.expenses.q2;
-appData.expenses.q3 = appData.expenses.q4;
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", ''),
+	a3 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a4 = prompt("Во сколько обойдется?", '');
 
-alert (appData.budget/30);
+appData.expenses.a1 = a2;
+appData.expenses.a3 = a4;
+
+alert(appData.budget / 30);
